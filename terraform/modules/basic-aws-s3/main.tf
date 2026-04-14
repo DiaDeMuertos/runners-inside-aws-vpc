55332a072450dc026aa9fda6233f3420cb9dcba5
jayceesnow <imworkingnow@msn.com>
2026-04-14 13:57:23 -0700

    refactor(go-test): just aws s3

--- terraform/modules/basic-aws-s3/main.tf
+++ terraform/modules/basic-aws-s3/main.tf
@@ -6,29 +6,29 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.name
 }
 
-resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
-  bucket = aws_s3_bucket.bucket.id
+# resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+#   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-# tfsec:ignore:aws-s3-encryption-customer-key
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-  bucket = aws_s3_bucket.bucket.id
+# # tfsec:ignore:aws-s3-encryption-customer-key
+# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+#   bucket = aws_s3_bucket.bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "bucket_versioning" {
+#   bucket = aws_s3_bucket.bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
