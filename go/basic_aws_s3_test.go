8ef453d6b4c5d9914aa7fb6850a0f3a4b34a63ee
jayceesnow <imworkingnow@msn.com>
2026-04-14 17:23:01 -0700

    feat(go-test): add aws s3 test

---
+++ go/basic_aws_s3_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAwsS3(t *testing.T) {
+	var bucketName string = fmt.Sprintf("terraform-up-and-running-state-%d", time.Now().Unix())
+
+	opts := &terraform.Options{
+		TerraformDir: "../terraform/test/basic-aws-s3/",
+
+		Vars: map[string]interface{}{
+			"name": fmt.Sprintf("%s", bucketName),
+		},
+	}
+	terraform.Init(t, opts)
+	terraform.Apply(t, opts)
+
+	outputName := terraform.OutputRequired(t, opts, "name")
+
+	assert.Equal(t, bucketName, outputName)
+
+	defer terraform.Destroy(t, opts)
+}
