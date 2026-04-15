3c570138d11c84af88dcf4d7233acffa1fd60d98
jayceesnow <imworkingnow@msn.com>
2026-04-14 17:07:46 -0700

    refactor(go-test): remove outputs read

--- go/basic_aws_s3_with_stages_test.go
+++ go/basic_aws_s3_with_stages_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"testing"
 	"time"
 
@@ -41,13 +40,13 @@ func TestAwsS3WithStages(t *testing.T) {
 	// Stage 2: Validate
 	test_structure.RunTestStage(t, "validate", func() {
 		t.Log("<-- Running validate stage -->")
-		terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+		// terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
 
-		fooOutput := strings.TrimSpace(terraform.Output(t, terraformOptions, "foo"))
-		t.Logf("[VALIDATE] LOG: foo=%s", fooOutput)
+		// fooOutput := strings.TrimSpace(terraform.Output(t, terraformOptions, "foo"))
+		// t.Logf("[VALIDATE] LOG: foo=%s", fooOutput)
 
-		bucketNameOutputRaw := strings.TrimSpace(terraform.Output(t, terraformOptions, "name"))
-		t.Logf("[VALIDATE] LOG: name:%s", bucketNameOutputRaw)
+		// bucketNameOutputRaw := strings.TrimSpace(terraform.Output(t, terraformOptions, "name"))
+		// t.Logf("[VALIDATE] LOG: name:%s", bucketNameOutputRaw)
 
 		// if bucketNameOutput == "" {
 		// 	t.Error("Bucket name output is empty!")
